1e488f3baa05ac3828fbc80562e830a1b674e26a
Dheeraj <dheeraj2834@gmail.com>
2026-07-13 18:06:47 +0000

    dev-2

--- Day-6/main.tf
+++ Day-6/main.tf
@@ -7,3 +7,14 @@ resource "aws_instance" "application" {
   }
   
 }
+
+resource "aws_vpc" "app_vpc" {
+
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "Dev"
+  }
+}
