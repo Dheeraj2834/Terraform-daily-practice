0e5958278c402cc99649797a628a409866c2c562
Bandaru Dheeraj <dheeraj2834@gmail.com>
2026-07-23 00:10:51 +0530

    provisioners practice

---
+++ Day-14-provisioners/local-exec/main.tf
@@ -0,0 +1,37 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_key_pair" "terraform_key" {
+  key_name   = "terraform-key"
+  public_key = file("C:/Users/Dell/.ssh/id_ed25519.pub")
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-01edba92f9036f76e"
+  instance_type = "t3.micro"
+
+  key_name = aws_key_pair.terraform_key.key_name
+
+  tags = {
+    Name = "pro"
+  }
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("C:/Users/Dell/.ssh/id_ed25519")
+    host        = self.public_ip
+  }
+
+  provisioner "local-exec" {
+    command = "echo ${self.public_ip} >> inventory"
+  }
+
+   provisioner "remote-exec" {
+    scripts = [
+      "test.sh",
+      "script.sh"
+    ]
+  }
+}
---
+++ Day-14-provisioners/local-exec/test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo yum install git -y
+sudo yum install python3-pip -y
